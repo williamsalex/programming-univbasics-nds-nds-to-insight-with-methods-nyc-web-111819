0297cf315aba9dc47328771a30c1df028d40a301
Alexander Williams <williams_alex@protonmail.com>
2019-11-15 23:45:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,14 +8,14 @@ require 'directors_database'
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
-def gross_for_director(director_data)
-  directorNest = directors_database[director]
-  total_gross = 0 
-  for y in directorNest[:movies] do
-    total_gross = total_gross + y[:worldwide_gross]
-  end
-  total_gross
-end
+#def gross_for_director(director_data)
+#  directorNest = directors_database[director]
+#  total_gross = 0 
+#  for y in directorNest[:movies] do
+#    total_gross = total_gross + y[:worldwide_gross]
+#  end
+#  total_gross
+#end
 
 def gross_for_director(director_data)
   hash = {}
