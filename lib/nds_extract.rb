9812287212d362cdb3beb11b146c52c4f01b7936
Alexander Williams <williams_alex@protonmail.com>
2019-11-15 23:47:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,22 +25,23 @@ def gross_for_director(director_data)
   total_gross
 end
 
-def directors_totals(nds)
-  hash = {}
-  for x in directors_database
-    total_gross = 0
-    for y in x[:movies]
-      total_gross = total_gross + y[:worldwide_gross]
-    end
-    hash[x[:name]] = total_gross
-  end
-  hash
-end
+#def directors_totals(nds)
+#  hash = {}
+#  for x in directors_database
+#    total_gross = 0
+#    for y in x[:movies]
+#      total_gross = total_gross + y[:worldwide_gross]
+#    end
+#    hash[x[:name]] = total_gross
+#  end
+#  hash
+#end
 
 def directors_totals(nds)
   hash = {}
   for x in nds do
     gross_for_director(x)
+    hash[x][:name] = total_gross
   end
-  
+  hash
 end
