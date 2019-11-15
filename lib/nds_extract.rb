ba470849c6caf28c1b663f1dbc62c8ee518fbad1
Alexander Williams <williams_alex@protonmail.com>
2019-11-15 23:28:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,8 +8,9 @@ require 'directors_database'
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
-def gross_for_director(director_data)
-  directors_database
+def gross_for_director(director)
+  directorNest = directors_database[director]
+  
 end
 
 def directors_totals(nds)
